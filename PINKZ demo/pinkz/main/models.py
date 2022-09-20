3e6b207e294496aac1dd2853f59c4e4da0e65f7f
dhusin <dhusin01@gmail.com>
2022-09-21 00:27:20 +0530

    home page modification

--- PINKZ demo/pinkz/main/models.py
+++ PINKZ demo/pinkz/main/models.py
@@ -1,3 +1,13 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    productId = models.AutoField(primary_key=True,unique=True)
+    productName = models.CharField(max_length=100)
+    productDesc = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(null = True, blank= True, upload_to='images/')
+
+    def __str__(self):
+        return self. productName
+    
--- PINKZ demo/pinkz/main/views.py
+++ PINKZ demo/pinkz/main/views.py
@@ -1,6 +1,9 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'products' : Product.objects.all()}
+
+    return render(request, 'home.html', context)
